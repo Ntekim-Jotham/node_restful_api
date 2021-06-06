a64ea32694da76ad0d9c18535dce709e28c3f347
Jotham-Ntekim <jothamweb@gmail.com>
2021-06-06 00:16:56 -0700

    added user CRUD resource

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ let http = require('http');
 let https = require('https');
 let url = require('url');
 let stringDecoder = require('string_decoder').StringDecoder;
-let config = require('./config');
+let config = require('./lib/config');
 let fs = require('fs');
+let handlers = require('./lib/handlers');
+let helpers = require('./lib/helpers');
+
 
 // Instantiate the HTTP server
-let httpServer = http.createServer(function(req,res) {
+let httpServer = http.createServer((req,res) => {
    unifiedServer(req, res);
 });
 
 // Start the server and have it listen on port 3000
-httpServer.listen(config.httpPort, function() {
+httpServer.listen(config.httpPort, () => {
   console.log("The server is listening on port: " +config.httpPort);
 });
 
@@ -26,17 +29,17 @@ let httpsServerOptions = {
    'key' : fs.readFileSync('./https/key.pem'),
    'cert': fs.readFileSync('./https/cert.pem')
 };
-let httpsServer = https.createServer(httpsServerOptions, function(req,res) {
+let httpsServer = https.createServer(httpsServerOptions, (req,res) => {
    unifiedServer(req, res);
 });
 
 // Start the server and have it listen on port 3000
-httpsServer.listen(config.httpsPort, function() {
+httpsServer.listen(config.httpsPort, () => {
   console.log("The server is listening on port: " +config.httpsPort);
 });
 
 //All the server logic for both http and https
-let unifiedServer = function(req, res){
+let unifiedServer = (req, res) => {
   // Get URL and parse it
   let parsedUrl = url.parse(req.url,true);
 
@@ -56,10 +59,10 @@ let unifiedServer = function(req, res){
   // Get the payload, if any
   let decoder = new stringDecoder('utf-8');
   let buffer = '';
-  req.on('data', function(data){
+  req.on('data', (data) => {
     buffer += decoder.write(data);
   });
-  req.on('end', function(){
+  req.on('end', () => {
     buffer += decoder.end();
 
     // Choose the handler this request should go to, if one not found, use the notFound handler
@@ -71,54 +74,36 @@ let unifiedServer = function(req, res){
       'queryStringObject' : queryStringObject,
       'method' : method,
       'headers' : headers,
-      'payload' : buffer
+      'payload' : helpers.parseJsonToObject(buffer)
     };
+    if (chosenHandler) {
+      // Route the request to handler specified in the router
+      chosenHandler(data, (statusCode, payload) => {
+        // Use the status code called back by the handler or set default to 200
+        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
 
-    // Route the request to handler specified in the router
-    chosenHandler(data, function(statusCode, payload){
-      // Use the status code called back by the handler or set default to 200
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+        // Use the payload called back by the handler or set default to empty object
+         payload = typeof(payload) == 'object' ? payload : {};
 
-      // Use the payload called back by the handler or set default to empty object
-       payload = typeof(payload) == 'object' ? payload : {};
+        // Convert the payload to string
+        let payloadString = JSON.stringify(payload);
 
-      // Convert the payload to string
-      let payloadString = JSON.stringify(payload);
+        // Return the response
+        res.setHeader('Content-Type', 'application/json');
+        res.writeHead(statusCode);
+        res.end(payloadString);
 
-      // Return the response
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(payloadString);
+         // Log the request path
+         console.log('Returning this response: ', statusCode, payloadString);
+      });
+    }else {
+      console.log('There is an error');
+    }
 
-       // Log the request path
-       console.log('Returning this response: ', statusCode, payloadString);
-    });
   });
-};
-
-// Define the message a user get on sending an HTTP request to the route /hello
-let welcomeMessage = {
-  'name' : 'Guest',
-  'message' : '',
-  'status'  : 200,
-
-  sendMessage(){
-    return this.message = 'Hello @' + this.name + ', ' + 'welcome to the Node.js Masterclass. It\'s great to have you on board.'
-  }
-};
-
-// Define the handlers
-let handlers = {};
-
-handlers.hello =  function (data, callback) {
-  callback(welcomeMessage.status, {'message' : welcomeMessage.sendMessage()});
-};
-
-handlers.notFound =  function (data, callback) {
-  callback(404);
 };
 
 // Define a request router
 let router = {
-  'hello' : handlers.hello
+  'users' : handlers.users
 }
